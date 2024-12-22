7dce3606d4acc0736c48ef759e2f0581b20039d5
joyantgupta <joyantsheikharguptajoy@gmail.com>
2024-12-22 07:39:24 +0600

    loader fix

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-
-
-
-
 var tl = gsap.timeline()
 tl.from(".line h1", {
     y:150,
@@ -9,7 +5,7 @@ tl.from(".line h1", {
     duration: 0.6,
     delay: 0.5 
 })
-tl.from("#line1-part1, .line h2", {
+tl.from("#line1-part1", {
     opacity: 0,
     onStart: function() {
         var h5Timer = document.querySelector("#line1-part1 h5")
@@ -23,10 +19,23 @@ tl.from("#line1-part1, .line h2", {
             
         }, 35)
     },
+});
+tl.to(".line h2", {
+    animationName: "anime",
+    opacity: 1
 })
-
 tl.to("#loader", {
     opacity: 0,
-    duration: 0.4,
-    delay: 4
+    duration: 0.2,
+    delay: 4,
+});
+tl.from("#page1", {
+    delay: 0.2,
+    y: 1600,
+    opacity: 1,
+    duration: 0.5,
+    ease: Power4
+})
+tl.to("#loader", {
+    display: "none"
 })
