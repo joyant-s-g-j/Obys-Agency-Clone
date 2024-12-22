664f6ad067d5291151ff5a51c16b93dc9621f03d
joyantgupta <joyantsheikharguptajoy@gmail.com>
2024-12-22 07:04:05 +0600

    loader done

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
-gsap.from(".line h1", {
+
+
+
+
+var tl = gsap.timeline()
+tl.from(".line h1", {
     y:150,
-    stagger: 0.2
+    stagger: 0.25,
+    duration: 0.6,
+    delay: 0.5 
+})
+tl.from("#line1-part1, .line h2", {
+    opacity: 0,
+    onStart: function() {
+        var h5Timer = document.querySelector("#line1-part1 h5")
+        var grow = 0
+        setInterval(function(){
+            if(grow < 100) {
+                h5Timer.innerHTML = grow++
+            } else{
+                h5Timer.innerHTML = grow
+            }
+            
+        }, 35)
+    },
+})
+
+tl.to("#loader", {
+    opacity: 0,
+    duration: 0.4,
+    delay: 4
 })
